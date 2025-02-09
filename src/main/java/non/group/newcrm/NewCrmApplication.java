0787e0c56c99bca1797115df6b467ac943654ade
fellon-dnb <fellon@ancevt.com>
2025-02-09 13:02:26 +0300

    Init commit

---
+++ src/main/java/non/group/newcrm/NewCrmApplication.java
@@ -0,0 +1,13 @@
+package non.group.newcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/non/group/newcrm/NewCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package non.group.newcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
